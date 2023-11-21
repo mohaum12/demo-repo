371a7592f1404eb74bc809d0057ccd67ae58c95f
mohaum12 <mohaum12@gmail.com>
2023-11-21 22:32:49 +0200

    updated the test

--- test.js
+++ test.js
@@ -3,8 +3,15 @@ const { openBrowser, goto, write, goBack, closeBrowser } = require('taiko');
     try {
         await openBrowser();
         await goto("google.com");
-        await write("people are awesome");
+        await write("Keychron K6");
+        await click("Keychron K6 Wireless Mechanical Keyboard")
+        // Select Hotswap option 
+        await click($("#swatch-option2 > div > fieldset > ul > li:nth-child(3) > div > div.swatch-button-title-text"))
+        // Select brown switch 
+        await click($("#swatch-option3 > div > fieldset > ul > li:nth-child(3) > div"))
+        await click("Add to cart")
         await goBack();
+        
     } catch (error) {
         console.error(error);
     } finally {
