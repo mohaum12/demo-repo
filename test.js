c8fca10645323c4073baa35e09ddb2a52638fd56
mohaum12 <mohaum12@gmail.com>
2023-11-21 22:52:11 +0200

    Refactored the test

--- test.js
+++ test.js
@@ -4,14 +4,11 @@ const { openBrowser, goto, write, goBack, closeBrowser } = require('taiko');
         await openBrowser();
         await goto("google.com");
         await write("Keychron K6");
+        await press("Enter")
         await click("Keychron K6 Wireless Mechanical Keyboard")
-        // Select Hotswap option 
-        await click($("#swatch-option2 > div > fieldset > ul > li:nth-child(3) > div > div.swatch-button-title-text"))
         // Select brown switch 
         await click($("#swatch-option3 > div > fieldset > ul > li:nth-child(3) > div"))
         await click("Add to cart")
-        await goBack();
-        
     } catch (error) {
         console.error(error);
     } finally {
